00c926786918d7ecb8012f7bab6bbd787f9a5636
Piyush Singh <b221037@iiit-bh.ac.in>
2025-06-20 11:46:50 +0530

    performed an test insertion and retriving

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new MongoClient("mongodb://localhost:27017");
+        var database = client.GetDatabase("testdb");
+        var collection = database.GetCollection<BsonDocument>("testcollection");
+
+        var doc = new BsonDocument { { "message", "Hello from C# and MongoDB!" } };
+        await collection.InsertOneAsync(doc);
+
+        var result = await collection.Find(new BsonDocument()).ToListAsync();
+        foreach (var item in result)
+        {
+            Console.WriteLine(item.ToJson());
+        }
+    }
+}
